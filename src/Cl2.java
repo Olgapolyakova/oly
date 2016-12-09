8b6f5891a1ca0e512b243d552119fa3cc3127298
Olgapolyakova <zenitkka@mail.ru>
2016-12-09 21:59:36 +0300

    проба пера

---
+++ src/Cl2.java
@@ -0,0 +1,11 @@
+/**
+ * Created by on 09.12.2016.
+ */
+public class Cl2 {
+    public static void main(String[] args) {
+
+
+        System.out.println("Hello word");
+        System.out.println("Olaala");
+    }
+}
